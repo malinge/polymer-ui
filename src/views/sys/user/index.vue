fceaf5457d2931f069df33be7fae997fbd57b75a
张西锋 <zhangxifeng@163.com>
2026-06-24 16:06:02 +0800

    代码生成

--- src/views/sys/user/index.vue
+++ src/views/sys/user/index.vue
@@ -2,47 +2,26 @@
   <el-row :gutter="6">
     <el-col :span="5">
       <el-card>
-        <DeptTree @node-click="handleDeptClick" />
+        <DeptTree @node-click="handleDeptClick"/>
       </el-card>
     </el-col>
     <el-col :span="19">
       <el-card>
         <el-form :inline="true" :model="state.queryForm" @keyup.enter="getDataList()">
           <el-form-item>
-            <el-input
-              v-model="state.queryForm.username"
-              placeholder="用户名"
-              clearable
-            ></el-input>
+            <el-input v-model="state.queryForm.username" placeholder="用户名" clearable/>
           </el-form-item>
           <el-form-item>
-            <el-input
-              v-model="state.queryForm.mobile"
-              placeholder="手机号"
-              clearable
-            ></el-input>
+            <el-input v-model="state.queryForm.mobile" placeholder="手机号" clearable/>
           </el-form-item>
-          <!--					<el-form-item>-->
-          <!--						<fast-select v-model="state.queryForm.gender" dict-type="user_gender" clearable placeholder="性别"></fast-select>-->
-          <!--					</el-form-item>-->
           <el-form-item>
             <el-button @click="getDataList()">查询</el-button>
           </el-form-item>
           <el-form-item>
-            <el-button
-              v-auth="'sys:user:save'"
-              type="primary"
-              @click="addOrUpdateHandle()"
-              >新增</el-button
-            >
+            <el-button v-auth="'sys:user:save'" type="primary" @click="addOrUpdateHandle()">新增</el-button>
           </el-form-item>
           <el-form-item>
-            <el-button
-              v-auth="'sys:user:delete'"
-              type="danger"
-              @click="deleteBatchHandle()"
-              >删除</el-button
-            >
+            <el-button v-auth="'sys:user:delete'" type="danger" @click="deleteBatchHandle()">删除</el-button>
           </el-form-item>
           <el-form-item v-auth="'sys:user:import'">
             <el-upload
@@ -50,131 +29,69 @@
                 :before-upload="beforeUpload"
                 :on-success="handleSuccess"
                 :on-error="handleError"
-                :show-file-list="false"
-            >
+                :show-file-list="false">
               <el-button type="info">导入</el-button>
             </el-upload>
           </el-form-item>
           <el-form-item>
-            <el-button v-auth="'sys:user:export'" type="success" @click="exportHandle()"
-              >导出</el-button
-            >
+            <el-button v-auth="'sys:user:export'" type="success" @click="exportHandle()">导出</el-button>
           </el-form-item>
         </el-form>
         <el-table
-          v-loading="state.dataListLoading"
-          show-overflow-tooltip
-          :data="state.dataList"
-          border
-          style="width: 100%"
-          ref="tableRef"
-          @select="selectHandle"
-          @select-all="selectAllHandle"
-        >
-          <el-table-column
-            type="selection"
-            header-align="center"
-            align="center"
-            width="50"
-          ></el-table-column>
-          <el-table-column
-            prop="username"
-            label="用户账号"
-            header-align="center"
-            align="center"
-          ></el-table-column>
-          <el-table-column
-            prop="realName"
-            label="用户姓名"
-            header-align="center"
-            align="center"
-          ></el-table-column>
-          <fast-table-column
-            prop="gender"
-            label="用户性别"
-            dict-type="user_gender"
-          ></fast-table-column>
-          <el-table-column
-            prop="mobile"
-            label="手机号码"
-            header-align="center"
-            align="center"
-          ></el-table-column>
-          <el-table-column
-            prop="deptName"
-            label="所属部门"
-            header-align="center"
-            align="center"
-          ></el-table-column>
-          <fast-table-column
-            prop="status"
-            label="用户状态"
-            dict-type="user_status"
-          ></fast-table-column>
-          <el-table-column
-            prop="createTime"
-            label="创建时间"
-            header-align="center"
-            align="center"
-          ></el-table-column>
-          <el-table-column
-            label="操作"
-            fixed="right"
-            header-align="center"
-            align="center"
-            width="120"
-          >
+            v-loading="state.dataListLoading"
+            show-overflow-tooltip
+            :data="state.dataList"
+            border
+            style="width: 100%"
+            ref="tableRef"
+            @select="selectHandle"
+            @select-all="selectAllHandle">
+          <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
+          <el-table-column prop="username" label="用户账号" header-align="center" align="center"></el-table-column>
+          <el-table-column prop="realName" label="用户姓名" header-align="center" align="center"></el-table-column>
+          <fast-table-column prop="gender" label="用户性别" dict-type="user_gender"></fast-table-column>
+          <el-table-column prop="mobile" label="手机号码" header-align="center" align="center"></el-table-column>
+          <el-table-column prop="deptName" label="所属部门" header-align="center" align="center"></el-table-column>
+          <fast-table-column prop="status" label="用户状态" dict-type="user_status"></fast-table-column>
+          <el-table-column prop="createTime" label="创建时间" header-align="center" align="center"></el-table-column>
+          <el-table-column label="操作" fixed="right" header-align="center" align="center" width="120">
             <template #default="scope">
-              <el-button
-                v-auth="'sys:user:update'"
-                type="primary"
-                link
-                @click="addOrUpdateHandle(scope.row.id)"
-                >修改</el-button
-              >
-              <el-button
-                v-auth="'sys:user:delete'"
-                type="primary"
-                link
-                @click="deleteBatchHandle(scope.row.id)"
-                >删除</el-button
-              >
+              <el-button v-auth="'sys:user:update'" type="primary" link @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
+              <el-button v-auth="'sys:user:delete'" type="primary" link @click="deleteBatchHandle(scope.row.id)">删除</el-button>
             </template>
           </el-table-column>
         </el-table>
         <el-pagination
-          :current-page="state.pageNo"
-          :page-sizes="state.pageSizes"
-          :page-size="state.pageSize"
-          :total="state.total"
-          layout="total, sizes, prev, pager, next, jumper"
-          @size-change="sizeChangeHandle"
-          @current-change="currentChangeHandle"
-        >
+            :current-page="state.pageNo"
+            :page-sizes="state.pageSizes"
+            :page-size="state.pageSize"
+            :total="state.total"
+            layout="total, sizes, prev, pager, next, jumper"
+            @size-change="sizeChangeHandle"
+            @current-change="currentChangeHandle">
         </el-pagination>
       </el-card>
     </el-col>
   </el-row>
-
   <!-- 弹窗, 新增 / 修改 -->
   <add-or-update ref="addOrUpdateRef" @refresh-data-list="getDataList"></add-or-update>
 </template>
 
 <script setup lang="ts" name="SysUserIndex">
-import { useCrud } from "@/hooks";
-import { reactive, ref} from "vue";
+import {useCrud} from "@/hooks";
+import {reactive, ref} from "vue";
 import AddOrUpdate from "./add-or-update.vue";
-import { IHooksOptions } from "@/hooks/interface";
-import { ElMessage, UploadProps } from "element-plus";
+import {IHooksOptions} from "@/hooks/interface";
+import {ElMessage, UploadProps} from "element-plus";
 import DeptTree from "./dept-tree.vue";
 import {importUserData} from "@/api/sys/user";
 import {useFileUpload} from "@/hooks/useFileUpload";
-const tableRef = ref();
 
+const tableRef = ref();
 const state: IHooksOptions = reactive({
   dataListUrl: "/sys/user/page",
   deleteUrl: "/sys/user",
-  exportUrl:"/sys/user/export",
+  exportUrl: "/sys/user/export",
   primaryKey: "id",
   queryForm: {
     username: "",
@@ -184,7 +101,7 @@ const state: IHooksOptions = reactive({
   },
 });
 
-const { uploadDataImport } = useFileUpload()
+const {uploadDataImport} = useFileUpload()
 const addOrUpdateRef = ref();
 const addOrUpdateHandle = (id?: number) => {
   addOrUpdateRef.value.init(id);
@@ -216,7 +133,7 @@ const handleSuccess: UploadProps["onSuccess"] = (res) => {
   });
 };
 
-const beforeUpload : UploadProps["beforeUpload"] = (file) => {
+const beforeUpload: UploadProps["beforeUpload"] = (file) => {
   // 只做必要的文件大小校验
   const isLt10M = file.size / 1024 / 1024 < 10
   if (!isLt10M) {
